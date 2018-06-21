0777281eaa410543410babd31166473636061fb9
Itsick <any.site.n.site@gmail.com>
2018-06-21 19:38:37 +0300

    add z-index to popup

--- js/extention-data.js
+++ js/extention-data.js
@@ -1,2 +1,2 @@
-const _extension={"id":"1click-stream-webcam","video":"GR6J3lFLrj4","chromeId":"none","name":"1click Webcam to Picture","short_description":"Get image file from your webcam","description":"","notPublished":true,"background":"#33AF3854"}
+const _extension={"id":"1click-stream-webcam","video":"GR6J3lFLrj4","chromeId":"none","name":"1Click Webcam to Picture","short_description":"Get image file from your webcam","description":"","notPublished":true,"background":"#33AF38"}
 
